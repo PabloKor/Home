93006c8fc46e9e9ba94554c077a7fe9af853636b
PabloKor <75683625+PabloKor@users.noreply.github.com>
2021-04-29 13:38:38 +0300

    Update car.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,12 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+
+from homewor_02.base import Vehicle
+
+
+class Car(Vehicle):
+    engine = 0
+
+    def set_engine(self, class_engine):
+        self.engine = class_engine
